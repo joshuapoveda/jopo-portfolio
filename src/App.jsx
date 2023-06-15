9a4ad5298f7e39cfb0489300515ab6328c32801e
Joshua Poveda <joshuapoveda9@gmail.com>
2023-06-14 22:00:06 -0400

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import "./App.css";
-import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 import Home from "./components/Home";
-import Nav from "./components/Nav"
-
+import Projects from "./components/Projects";
+import Github from "./components/Github";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div>
-        <Nav></Nav>
-        <Home></Home>
-        <Footer></Footer>
+      <div className="flex flex-col h-screen justify-between border-4 border-red-700">
+        <BrowserRouter>
+          <Nav></Nav>
+          <div className="border-4 h-full border-blue-700">
+            <Routes>
+              <Route path="/" element={<Home />} />
+
+              <Route path="/projects" element={<Projects />} />
+
+              <Route path="/github" element={<Github />} />
+
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </div>
+          <Footer></Footer>
+        </BrowserRouter>
       </div>
     </>
   );
